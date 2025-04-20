dcd7e2a711df0f9955513369ba53431e88813d73
Phan Son <minikie456@gmail.com>
2025-04-21 00:01:20 +0700

    1. Finish 19-04-2025

--- 19-04-2025/27_RemoveElement.cpp
+++ 19-04-2025/27_RemoveElement.cpp
@@ -6,3 +6,37 @@ https://leetcode.com/problems/remove-element/description/?envType=study-plan-v2&
 /*
 Author: Minikie - Phan NT Son
 */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int removeElement(vector<int> &nums, int val)
+    {
+        for (auto it = nums.begin(); it != nums.end();){
+            if (*it == val){
+                nums.erase(it);
+            } else {
+                ++it;
+            }
+        }
+        return nums.size();
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> nums = {0,1,2,2,3,0,4,2};
+    int val = 2;
+    int k = s.removeElement(nums,val);
+    
+    for (int i : nums){
+        cout << i << " ";
+    }
+    cout << endl << k;
+    return 0;
+}
---
+++ 20-04-2025/80_RemoveDupFromArray2.cpp
@@ -0,0 +1,25 @@
+/*
+Problem:https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/?envType=study-plan-v2&envId=top-interview-150
+
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    // Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
