33534ac131181d66105313d1bc68f77dcb62ab30
Phan Son <minikie456@gmail.com>
2025-04-23 00:13:38 +0700

    Temp: Pick problem for 23-04-2025

---
+++ 23-04-2025/45_JumpGame2.cpp
@@ -0,0 +1,32 @@
+/*
+Problem:https://leetcode.com/problems/jump-game-ii/description/?envType=study-plan-v2&envId=top-interview-150
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class Solution
+{
+public:
+    int jump(vector<int> &nums)
+    {
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
