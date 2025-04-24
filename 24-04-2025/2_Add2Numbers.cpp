42f2ca26c1369baa3cbe0d1d35cc2c4750a098b0
Phan Son <minikie456@gmail.com>
2025-04-25 00:09:57 +0700

    7. Finish #2 problem of 24-04-2025: 2 - Add Two Numbers

--- 24-04-2025/2_Add2Numbers.cpp
+++ 24-04-2025/2_Add2Numbers.cpp
@@ -62,40 +62,137 @@ struct LinkedList
 class Solution
 {
 public:
+    /*
+    Explain #1 Solution:
+        tHead       is a dummy node that helps build the result.
+        tCur        is the current node in the result list.
+        tail        is used to track the last valid node.
+        remain      holds the carry (like the digit overflow when summing).
+
+        Process like normal, if have carry then have to process it.
+    */
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
     {
         int remain = 0;
-        int mul = 1;
-        int total = 0;
+        ListNode *tHead = new ListNode();
+        ListNode *tCur = tHead;
+        ListNode *tail = tCur;
         while (l1 != nullptr && l2 != nullptr)
         {
-            total += ((l1->val + l2->val) % 10 + remain) * mul;
-            remain = (l1->val + l2->val) / 10;
-
-            mul *= 10;
+            tCur->val = (l1->val + l2->val + remain) % 10;
+            ListNode *newDigit = new ListNode();
+            remain = (l1->val + l2->val + remain) / 10;
+            tCur->next = newDigit;
+            tail = tCur;
+            tCur = tCur->next;
             l1 = l1->next;
             l2 = l2->next;
         }
-        while (l1->next != nullptr)
+        while (l1 != nullptr)
         {
-            total += l1->val * mul;
-            mul *= 10;
+            tCur->val = (l1->val + remain) % 10;
+            ListNode *newDigit = new ListNode();
+            remain = (l1->val + remain) / 10;
+            tCur->next = newDigit;
+            tail = tCur;
+            tCur = tCur->next;
+            l1 = l1->next;
         }
-        while (l2->next != nullptr)
+        while (l2 != nullptr)
         {
-            total += l2->val * mul;
-            mul *= 10;
+            tCur->val = (l2->val + remain) % 10;
+            ListNode *newDigit = new ListNode();
+            remain = (l2->val + remain) / 10;
+            tCur->next = newDigit;
+            tail = tCur;
+            tCur = tCur->next;
+            l2 = l2->next;
         }
-        return total;
+        if (remain > 0)
+        {
+            tCur->val = remain;
+            tail = tCur;
+        }
+        tail->next = nullptr;
+        return tHead;
+    }
+
+    /*
+    Explain #2 Solution:
+        Using only 1 loop, ends only when both l1 and l2 are reach 
+        the end and no remain carry.
+        In each loop, check if list reach it's end or not, it so
+        then add to sum to process.
+    */
+    ListNode *addTwoNumbers2(ListNode *l1, ListNode *l2)
+    {
+        ListNode *dummy = new ListNode(0);
+        ListNode *current = dummy;
+        int carry = 0;
+
+        while (l1 || l2 || carry)
+        {
+            int sum = carry;
+            if (l1)
+            {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2)
+            {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            current->next = new ListNode(sum % 10);
+            carry = sum / 10;
+            current = current->next;
+        }
+
+        return dummy->next;
     }
 };
 
 int main()
 {
     Solution s;
+    LinkedList l1;
+    LinkedList l2;
     while (true)
     {
+        int n1, n2;
+        cout << "Size l1: ";
+        cin >> n1;
+        cout << "Size l2: ";
+        cin >> n2;
 
+        cout << "---- 1 -----" << endl;
+        for (int i = 0; i < n1; i++)
+        {
+            int input;
+            cout << "[" << i << "]";
+            cin >> input;
+            ListNode *newNode = new ListNode(input);
+            l1.addNew(newNode);
+        }
+        cout << "---- 2 -----" << endl;
+        for (int i = 0; i < n2; i++)
+        {
+            int input;
+            cout << "[" << i << "]";
+            cin >> input;
+            ListNode *newNode = new ListNode(input);
+            l2.addNew(newNode);
+        }
+
+        cout << "Answer: ";
+        ListNode *cur = s.addTwoNumbers(l1.head, l2.head);
+        while (cur != nullptr)
+        {
+            cout << cur->val;
+            cur = cur->next;
+        }
+        cout << endl;
         char x;
         cout << "Continue ? Y/N:";
         cin >> x;
@@ -103,6 +200,8 @@ int main()
         {
             break;
         }
+        l1.clear();
+        l2.clear();
     }
     return 0;
 }
