179b3917492edcc3eef7ada3679e197b40fc30e9
Phan Son <minikie456@gmail.com>
2025-04-25 17:03:48 +0700

    8. Finish #1 problem of 25-05-2025: 383 - RansomNote

--- 25-04-2025/383_RansomNote.cpp
+++ 25-04-2025/383_RansomNote.cpp
@@ -5,13 +5,67 @@ Problem:https://leetcode.com/problems/ransom-note/description/?envType=study-pla
 Author: Minikie - Phan NT Son
 */
 #include <iostream>
-
+#include <map>
+#include <unordered_map>
 using namespace std;
 class Solution
 {
 public:
+    /*
+    Explain #1 Solution:
+        Hash Map approach.
+        
+        - Count how many times each character appears in 'magazine' using a hash map.
+        - Then, iterate through each character in 'ransomNote':
+        - Decrease the count in the map.
+        - If the count becomes negative, it means 'magazine' doesn't have enough of that character.
+        - Return false in that case.
+        - If all characters in 'ransomNote' are available in sufficient amount, return true.
+    */
     bool canConstruct(string ransomNote, string magazine)
     {
+        map<char, int> maga;
+        for (char x : magazine)
+        {
+            maga[x] += 1;
+        }
+        for (char x : ransomNote)
+        {
+            maga[x] -= 1;
+            if (maga[x] < 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /*
+    Explain #2 Solution:
+        Same logic as above but use unordered_map because this map 
+        is not sort anything, therefore, speed up the time access 
+        element.`
+    */
+    bool canConstruct2(string ransomNote, string magazine)
+    {
+        if (ransomNote.length() > magazine.length())
+        {
+            return false;
+        }
+
+        unordered_map<char, int> maga;
+        for (char c : magazine)
+        {
+            ++maga[c];
+        }
+
+        for (char c : ransomNote)
+        {
+            if (--maga[c] < 0)
+                return false;
+        }
+
+        return true;
     }
 };
 
@@ -20,6 +74,12 @@ int main()
     Solution s;
     while (true)
     {
+        string in1, in2;
+        cout << "Enter ransomNote: ";
+        cin >> in1;
+        cout << "Enter magazine: ";
+        cin >> in2;
+        cout << "Answer: " << s.canConstruct2(in1, in2) << endl;
         char x;
         cout << "Continue ? Y/N:";
         cin >> x;
