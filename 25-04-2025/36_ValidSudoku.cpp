d6c7d81c3e41a854e5041fab4041d72ae5aed70d
Phan Son <minikie456@gmail.com>
2025-04-25 17:05:30 +0700

    Temp: picking up #2 problem for 25-04-2025

---
+++ 25-04-2025/36_ValidSudoku.cpp
@@ -0,0 +1,31 @@
+/*
+Problem:https://leetcode.com/problems/valid-sudoku/description/?envType=study-plan-v2&envId=top-interview-150
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    bool isValidSudoku(vector<vector<char>> &board)
+    {
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
