376a8a90d7dad74e890ceafbc1a58c54aa3a512c
Phan Son <minikie456@gmail.com>
2025-04-26 23:38:22 +0700

    Temp: Picking up #1 problem of 27-04-2025

---
+++ 27-04-2025/53_MaximumSubarray.cpp
@@ -0,0 +1,31 @@
+/*
+Problem:https://leetcode.com/problems/maximum-subarray/?envType=study-plan-v2&envId=top-interview-150
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
