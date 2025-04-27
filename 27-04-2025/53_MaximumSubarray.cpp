7c10a6fc9b55c4581c866ad757ff756f61caf298
Phan Son <minikie456@gmail.com>
2025-04-28 00:11:54 +0700

    10. Finish #1 problem of 27-04-2025: 53 - Masimum Subarray

--- 27-04-2025/53_MaximumSubarray.cpp
+++ 27-04-2025/53_MaximumSubarray.cpp
@@ -10,8 +10,37 @@ using namespace std;
 class Solution
 {
 public:
+    /*
+    Explain #1 Solution:
+        Using Kadane's algorithm to find the maximum subarray sum.
+
+        The algorithm decides whether to extend the current subarray
+        or to start a new subarray at the current index. It starts a
+        new subarray if adding the current element makes the sum smaller
+        than starting fresh from the current element itself.
+
+        Kadane’s checks:
+            maxCur = max(nums[i], maxCur + nums[i]);
+        where:
+            - maxCur: the maximum sum of subarrays ending at the current index
+            - maxSum: the maximum sum found overall
+
+        This works based on the idea that a bad prefix should be dropped
+        to maximize the sum.
+    */
     int maxSubArray(vector<int> &nums)
     {
+        int maxSum = nums[0], maxCur = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            maxCur = max(nums[i], maxCur + nums[i]);
+
+            if (maxCur > maxSum)
+            {
+                maxSum = maxCur;
+            }
+        }
+        return maxSum;
     }
 };
 int main()
@@ -19,6 +48,14 @@ int main()
     Solution s;
     while (true)
     {
+        int n;
+        cin >> n;
+        vector<int> nums(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> nums[i];
+        }
+        cout << "Answer: " << s.maxSubArray(nums) << "\n";
         char x;
         cout << "Continue ? Y/N:";
         cin >> x;
