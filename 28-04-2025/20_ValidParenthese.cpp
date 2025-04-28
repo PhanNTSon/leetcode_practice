32abe4c44e775f851d07a763578f9ad2bb0bfb75
Phan Son <minikie456@gmail.com>
2025-04-28 17:03:06 +0700

    Temp: picking up #2 problem for 28-04-2025

---
+++ 28-04-2025/20_ValidParenthese.cpp
@@ -0,0 +1,31 @@
+/*
+Problem:https://leetcode.com/problems/valid-parentheses/description/?envType=study-plan-v2&envId=top-interview-150
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+
+using namespace std;
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
