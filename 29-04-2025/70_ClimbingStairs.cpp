aae70d5db23a09d34668ea77bb7b7d73dee3e78c
Phan Son <minikie456@gmail.com>
2025-04-29 01:28:55 +0700

    Temp: Picking up problem #1 for 29-04-2025

---
+++ 29-04-2025/70_ClimbingStairs.cpp
@@ -0,0 +1,32 @@
+/*
+Problem:https://leetcode.com/problems/climbing-stairs/?envType=study-plan-v2&envId=top-interview-150
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+
+using namespace std;
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+    }
+};
+
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
