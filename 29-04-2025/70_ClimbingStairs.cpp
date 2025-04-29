7134061fc740fa31648757967832914ce3dfc5b9
Phan Son <minikie456@gmail.com>
2025-04-29 22:42:51 +0700

    14. Finish #1 problem of 29-04-2025: 70 - Climbing Stairs

--- 29-04-2025/70_ClimbingStairs.cpp
+++ 29-04-2025/70_ClimbingStairs.cpp
@@ -5,13 +5,26 @@ Problem:https://leetcode.com/problems/climbing-stairs/?envType=study-plan-v2&env
 Author: Minikie - Phan NT Son
 */
 #include <iostream>
+#include <vector>
 
 using namespace std;
 class Solution
 {
 public:
+    /*
+    Explain Solution:
+        This is another version of Fibonacci problem.
+    */
     int climbStairs(int n)
     {
+        int mem[n + 1];
+        mem[0] = 1;
+        mem[1] = 1;
+        for (int i = 2; i < n + 1; i++)
+        {
+            mem[i] = mem[i - 1] + mem[i - 2];
+        }
+        return mem[n];
     }
 };
 
@@ -20,6 +33,9 @@ int main()
     Solution s;
     while (true)
     {
+        int n;
+        cin >> n;
+        cout << "Answer: " << s.climbStairs(n) << "\n";
         char x;
         cout << "Continue ? Y/N:";
         cin >> x;
