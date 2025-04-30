3583359ca901b6d7c1b4342505987c7333379495
Phan Son <minikie456@gmail.com>
2025-04-30 10:51:01 +0700

    Temp: Picking up #2 problem of 30-04-2025

---
+++ 30-04-2025/1295_FindNumbers.cpp
@@ -0,0 +1,32 @@
+/*
+Problem:https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/?envType=daily-question&envId=2025-04-30
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class Solution
+{
+public:
+    int findNumbers(vector<int> &nums)
+    {
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
