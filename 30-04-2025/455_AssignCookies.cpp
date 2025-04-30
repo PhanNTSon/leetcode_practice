b8f19047167a266293194702a83f0265f35a90e7
Phan Son <minikie456@gmail.com>
2025-04-30 09:49:03 +0700

    Happy Reunification Day (30/04/1975 - 30/04/2025). Temp: Picking #1 problem of 30-04-2025.

---
+++ 30-04-2025/455_AssignCookies.cpp
@@ -0,0 +1,32 @@
+/*
+Problem:https://leetcode.com/problems/assign-cookies/description/
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class Solution
+{
+public:
+    int findContentChildren(vector<int> &g, vector<int> &s)
+    {
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
