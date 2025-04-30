545db7084c8c44a320148f2471bad8858b791eb3
Phan Son <minikie456@gmail.com>
2025-05-01 00:07:55 +0700

    Temp: picking up problem #1 for 01-05-2025

---
+++ 05-2025/01-05-2025/198_HouseRobber.cpp
@@ -0,0 +1,31 @@
+/*
+Problem:https://leetcode.com/problems/house-robber/description/
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    int rob(vector<int> &nums)
+    {
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
