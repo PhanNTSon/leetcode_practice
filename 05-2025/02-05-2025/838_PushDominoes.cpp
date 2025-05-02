1a06b8dba99512c84419e8c5b2abc28c28261051
Phan Son <minikie456@gmail.com>
2025-05-02 08:00:06 +0700

    Temp: Picking up #1 problem of 02-05-2025

---
+++ 05-2025/02-05-2025/838_PushDominoes.cpp
@@ -0,0 +1,31 @@
+/*
+Problem:https://leetcode.com/problems/push-dominoes/description/?envType=daily-question&envId=2025-05-02
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+
+using namespace std;
+class Solution
+{
+public:
+    string pushDominoes(string dominoes)
+    {
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
