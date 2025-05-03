43b7ca5a8e70fd00a523979474f9cbe10f8307d6
Phan Son <minikie456@gmail.com>
2025-05-03 08:33:39 +0700

    Temp: Picking #1 problem of 03-05-2025

---
+++ 05-2025/03-05-2025/1007_MinimumDomino.cpp
@@ -0,0 +1,32 @@
+/*
+Problem:https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/description/?envType=daily-question&envId=2025-05-03
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class Solution
+{
+public:
+    int minDominoRotations(vector<int> &tops, vector<int> &bottoms)
+    {
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
