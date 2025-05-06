290644bf0e3753f8c803d9eef5e762ff0aa6caac
Phan Son <minikie456@gmail.com>
2025-05-06 08:32:43 +0700

    Temp: picking #2 problem of 06-05-2025

---
+++ 05-2025/06-05-2025/605_CanPlaceFlower.cpp
@@ -0,0 +1,31 @@
+/*
+Problem:https://leetcode.com/problems/can-place-flowers/description/
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    bool canPlaceFlowers(vector<int> &flowerbed, int n)
+    {
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
