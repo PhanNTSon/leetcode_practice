fb47d5e21f29b278767c57561a0e4a4ece830dd1
Phan Son <minikie456@gmail.com>
2025-05-06 08:20:13 +0700

    Temp: picking #1 problem of 06-05-2025

---
+++ 05-2025/06-05-2025/1920_BuildArray.cpp
@@ -0,0 +1,31 @@
+/*
+Problem:https://leetcode.com/problems/build-array-from-permutation/description/?envType=daily-question&envId=2025-05-06
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> buildArray(vector<int> &nums)
+    {
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
