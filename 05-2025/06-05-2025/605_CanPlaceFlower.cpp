1c062eaf9a2c2f86cbd2114b1dcd3e7773eaa2b6
Phan Son <minikie456@gmail.com>
2025-05-06 11:07:25 +0700

    Finish #2 problem of 05-06-2025: 605 - Can Place Flowers

--- 05-2025/06-05-2025/605_CanPlaceFlower.cpp
+++ 05-2025/06-05-2025/605_CanPlaceFlower.cpp
@@ -10,8 +10,61 @@ using namespace std;
 class Solution
 {
 public:
+    /*
+    Explain Solution:
+        This problem is simple, just imitate the planting. Carefull about the
+        edge of the array.
+        Treat it seperately.
+    */
     bool canPlaceFlowers(vector<int> &flowerbed, int n)
     {
+        if (n == 0)
+        {
+            return true;
+        }
+        else if (flowerbed.size() == 1 && flowerbed[0] == 0)
+        {
+            if (n > 1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        for (int i = 0; i < flowerbed.size(); i++)
+        {
+            if (flowerbed[i] == 0)
+            {
+                if (i == 0)
+                {
+                    if (flowerbed[i + 1] == 0)
+                    {
+                        --n;
+                        flowerbed[i] = 1;
+                    }
+                }
+                else if (i == flowerbed.size() - 1)
+                {
+                    if (flowerbed[i - 1] == 0)
+                    {
+                        --n;
+                        flowerbed[i] = 1;
+                    }
+                }
+                else if (flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0)
+                {
+                    --n;
+                    flowerbed[i] = 1;
+                }
+            }
+            if (n == 0)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 };
 int main()
@@ -19,6 +72,18 @@ int main()
     Solution s;
     while (true)
     {
+        int size, n;
+        cout << "Size: ";
+        cin >> size;
+        cout << "n flowers: ";
+        cin >> n;
+        vector<int> flowerbed(size);
+        cout << "Flowerbed: ";
+        for (int i = 0; i < size; i++)
+        {
+            cin >> flowerbed[i];
+        }
+        cout << "Answer: " << (s.canPlaceFlowers(flowerbed, n) ? "Yes " : "No ") << "\n";
         char x;
         cout << "Continue ? Y/N:";
         cin >> x;
