a3d733716e817848c495ef8db3622d19aa6f94da
Phan Son <minikie456@gmail.com>
2025-05-07 09:07:01 +0700

    Temp: picking #1 problem of 07-05-2025

---
+++ 05-2025/07-05-2025/3341_FindMinimumTime.cpp
@@ -0,0 +1,33 @@
+/*
+Problem:https://leetcode.com/problems/find-minimum-time-to-reach-last-room-i/description/?envType=daily-question&envId=2025-05-07
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+class Solution
+{
+public:
+    int minTimeToReach(vector<vector<int>> &moveTime)
+    {
+        
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
