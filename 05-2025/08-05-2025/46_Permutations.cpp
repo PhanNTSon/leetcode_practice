57c14045e0230a15999ec5e4b1454ddce39053e1
Phan Son <minikie456@gmail.com>
2025-05-08 07:02:21 +0700

    Temp: picking #1 problem of 08-05-2025

---
+++ 05-2025/08-05-2025/46_Permutations.cpp
@@ -0,0 +1,31 @@
+/*
+Problem:https://leetcode.com/problems/permutations/description/
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    vector<vector<int>> permute(vector<int> &nums)
+    {
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
