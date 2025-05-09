255c21f183ababe8c6ddc0dc4e78f56340360208
Phan Son <minikie456@gmail.com>
2025-05-09 08:20:23 +0700

    Temp: picking up #1 problem for 09-05-2025

---
+++ 05-2025/09-05-2025/322_CoinChange.cpp
@@ -0,0 +1,31 @@
+/*
+Problem:https://leetcode.com/problems/coin-change/description/
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    int coinChange(vector<int> &coins, int amount)
+    {
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
