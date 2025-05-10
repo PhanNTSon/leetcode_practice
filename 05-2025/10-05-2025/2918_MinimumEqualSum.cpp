884b9680c4631091e06dece34df935a53a0dba81
Phan Son <minikie456@gmail.com>
2025-05-10 20:30:42 +0700

    Temp: picking #1 problem of 10-05-2025

---
+++ 05-2025/10-05-2025/2918_MinimumEqualSum.cpp
@@ -0,0 +1,31 @@
+/*
+Problem:https://leetcode.com/problems/minimum-equal-sum-of-two-arrays-after-replacing-zeros/description/?envType=daily-question&envId=2025-05-10
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    long long minSum(vector<int> &nums1, vector<int> &nums2)
+    {
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
