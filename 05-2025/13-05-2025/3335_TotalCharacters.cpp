7757eb962bc33a6ef7bc1091265946275206c221
Phan Son <minikie456@gmail.com>
2025-05-13 07:33:26 +0700

    Temp: picking up problem of 13-05-2025

---
+++ 05-2025/13-05-2025/3335_TotalCharacters.cpp
@@ -0,0 +1,30 @@
+/*
+Problem:https://leetcode.com/problems/total-characters-in-string-after-transformations-i/description/?envType=daily-question&envId=2025-05-13
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+
+using namespace std;
+class Solution {
+public:
+    int lengthAfterTransformations(string s, int t) {
+        
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
