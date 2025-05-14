9bc947ba6db5f6b990cbb3364f4ec9a838cccde9
Phan Son <minikie456@gmail.com>
2025-05-14 15:07:46 +0700

    Temp: picking #1 problem of 14-05-2025

---
+++ 05-2025/14-05-2025/77_Combinations.cpp
@@ -0,0 +1,31 @@
+/*
+Problem:https://leetcode.com/problems/combinations/description/
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    vector<vector<int>> combine(int n, int k)
+    {
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
