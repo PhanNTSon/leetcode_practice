4cfaab6ff2553ccc7074fa7a2eaff640f674f181
Phan Son <minikie456@gmail.com>
2025-05-16 16:05:29 +0700

    Temp: picking #1 problem of 16-05-2025

---
+++ 05-2025/16-05-2025/2901_LongestUnequal.cpp
@@ -0,0 +1,31 @@
+/*
+Problem:https://leetcode.com/problems/longest-unequal-adjacent-groups-subsequence-ii/description/?envType=daily-question&envId=2025-05-16
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    vector<string> getWordsInLongestSubsequence(vector<string> &words, vector<int> &groups)
+    {
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
