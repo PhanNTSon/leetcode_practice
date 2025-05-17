9e3e9f258c65f7b85a2dfd281897e6195367a78a
Phan Son <minikie456@gmail.com>
2025-05-17 18:09:51 +0700

    Temp: picking up problem of 17-05-2025

---
+++ 05-2025/17-05-2025/75_SortColors.cpp
@@ -0,0 +1,31 @@
+/*
+Problem:https://leetcode.com/problems/sort-colors/description/?envType=daily-question&envId=2025-05-17
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
