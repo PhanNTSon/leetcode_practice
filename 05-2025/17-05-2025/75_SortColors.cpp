b47de20a9275b2e3abfd6aef83d9b125d2fee641
Phan Son <minikie456@gmail.com>
2025-05-17 22:32:48 +0700

    36. Finish problem of 17-05-2025: 75 - Sort Colors

--- 05-2025/17-05-2025/75_SortColors.cpp
+++ 05-2025/17-05-2025/75_SortColors.cpp
@@ -10,8 +10,47 @@ using namespace std;
 class Solution
 {
 public:
+    /*
+    Explain Solution:
+        Using Quicksort Algorithm.
+        Quicksort Algorithm use an Element in array as a pivot and 
+        base on that Pivot element, It will toss out which one is 
+        smaller than pivot to the left, larger to the right.
+
+        After that, repeat the same process until the sub-array
+        length is 1.
+    */
     void sortColors(vector<int> &nums)
     {
+        qSort(nums, 0, nums.size() - 1);
+    }
+
+    void qSort(vector<int> &arr, int start, int end)
+    {
+        if (start < end)
+        {
+            int pivot = start;
+            int storeIndex = pivot + 1;
+            for (int i = start + 1; i <= end; i++)
+            {
+                if (arr[i] < arr[pivot])
+                {
+                    swap(arr[i], arr[storeIndex]);
+                    ++storeIndex;
+                }
+            }
+
+            swap(arr[pivot], arr[storeIndex - 1]);
+            qSort(arr, start, storeIndex - 1);
+            qSort(arr, storeIndex, end);
+        }
+    }
+    void swap(int &a, int &b)
+    {
+        if (&a == &b) return;
+        a = a + b;
+        b = a - b;
+        a = a - b;
     }
 };
 int main()
@@ -19,6 +58,24 @@ int main()
     Solution s;
     while (true)
     {
+        int n;
+        cout << "Size: ";
+        cin >> n;
+        vector<int> nums(n);
+        for (int i = 0; i < n; i++)
+        {
+            cout << "[" << i << "]: ";
+            cin >> nums[i];
+        }
+
+        s.sortColors(nums);
+        cout << "Answer: ";
+        for (int i : nums)
+        {
+            cout << i << ", ";
+        }
+
+        cout << "\n";
         char x;
         cout << "Continue ? Y/N:";
         cin >> x;
