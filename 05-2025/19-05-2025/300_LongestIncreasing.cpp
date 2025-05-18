dd7a9913c2f92399a2adccb4fe8e4ac051f72cac
Phan Son <minikie456@gmail.com>
2025-05-18 11:04:08 +0700

    37. Finish problem of 18-05-2025: 300 - Longest Increasing Subsequence

--- 05-2025/19-05-2025/300_LongestIncreasing.cpp
+++ 05-2025/19-05-2025/300_LongestIncreasing.cpp
@@ -10,8 +10,28 @@ using namespace std;
 class Solution
 {
 public:
+    /*
+    Explain Solution:
+        Use DP approach to solve this problem.
+        DP array is use to store the maximum length of a sub-sequence
+        that end at the current nums.
+    */
     int lengthOfLIS(vector<int> &nums)
     {
+        vector<int> dp(nums.size(), 1);
+        int maxLen = -1;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[j] < nums[i])
+                {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLen = max(maxLen, dp[i]);
+        }
+        return maxLen;
     }
 };
 int main()
@@ -19,6 +39,18 @@ int main()
     Solution s;
     while (true)
     {
+        int n;
+        cout << "Size: ";
+        cin >> n;
+        vector<int> nums(n);
+        for (int i = 0; i < n; i++)
+        {
+            cout << "[" << i << "]: ";
+            cin >> nums[i];
+        }
+
+        cout << "Asnwer: " << s.lengthOfLIS(nums) << "\n";
+
         char x;
         cout << "Continue ? Y/N:";
         cin >> x;
