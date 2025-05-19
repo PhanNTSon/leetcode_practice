2f09fe1b2a1f408c0e3eb3498f4942ebe218e299
Phan Son <minikie456@gmail.com>
2025-05-19 15:21:12 +0700

    38. Finish problem of 19-05-2025: 3024 - Type of Triangle

--- 05-2025/16-31/19-05-2025/3024_TypeOfTriangle.cpp
+++ 05-2025/16-31/19-05-2025/3024_TypeOfTriangle.cpp
@@ -12,6 +12,26 @@ class Solution
 public:
     string triangleType(vector<int> &nums)
     {
+        int a, b, c;
+        a = nums[0];
+        b = nums[1];
+        c = nums[2];
+        if (a + b <= c || a + c <= b || b + c <= a)
+        {
+            return "none";
+        }
+        if (a == b && b == c)
+        {
+            return "equilateral";
+        }
+        if (a == b || b == c || a == c)
+        {
+            return "isosceles";
+        }
+        else
+        {
+            return "scalene";
+        }
     }
 };
 int main()
@@ -19,6 +39,16 @@ int main()
     Solution s;
     while (true)
     {
+        int n;
+        cout << "Size: ";
+        cin >> n;
+        vector<int> nums(n);
+        for (int i = 0; i < n; i++)
+        {
+            cout << "[" << i << "]: ";
+            cin >> nums[i];
+        }
+        cout << "Answer: " << s.triangleType(nums) << "\n";
         char x;
         cout << "Continue ? Y/N:";
         cin >> x;
