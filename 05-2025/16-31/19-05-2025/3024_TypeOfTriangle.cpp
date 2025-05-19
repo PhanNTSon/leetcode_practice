0991d9f4928109bf23e881a19592e31473e03077
Phan Son <minikie456@gmail.com>
2025-05-19 14:58:17 +0700

    Reconstruct project system and picking problem of 19-05-2025

---
+++ 05-2025/16-31/19-05-2025/3024_TypeOfTriangle.cpp
@@ -0,0 +1,31 @@
+/*
+Problem:https://leetcode.com/problems/type-of-triangle/description/?envType=daily-question&envId=2025-05-19
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    string triangleType(vector<int> &nums)
+    {
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
