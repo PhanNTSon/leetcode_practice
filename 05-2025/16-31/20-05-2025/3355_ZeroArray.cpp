06ea9965f7158e23b6f431c84aa4815cc1d1b725
Phan Son <minikie456@gmail.com>
2025-05-20 20:03:20 +0700

    Temp: picking up problem of 20-05-2025

---
+++ 05-2025/16-31/20-05-2025/3355_ZeroArray.cpp
@@ -0,0 +1,31 @@
+/*
+Problem:https://leetcode.com/problems/zero-array-transformation-i/description/?envType=daily-question&envId=2025-05-20
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    bool isZeroArray(vector<int> &nums, vector<vector<int>> &queries)
+    {
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
