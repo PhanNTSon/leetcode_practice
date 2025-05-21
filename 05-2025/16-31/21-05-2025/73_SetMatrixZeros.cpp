6fb91d9b5dca6acaa5702690a79b699f2b31c594
Phan Son <minikie456@gmail.com>
2025-05-21 16:00:09 +0700

    Temp: picking up problem of 21-05-2025

---
+++ 05-2025/16-31/21-05-2025/73_SetMatrixZeros.cpp
@@ -0,0 +1,31 @@
+/*
+Problem:https://leetcode.com/problems/set-matrix-zeroes/description/?envType=daily-question&envId=2025-05-21
+*/
+/*
+Author: Minikie - Phan NT Son
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+    }
+};
+int main()
+{
+    Solution s;
+    while (true)
+    {
+        char x;
+        cout << "Continue ? Y/N:";
+        cin >> x;
+        if (x == 'n' || x == 'N')
+        {
+            break;
+        }
+    }
+    return 0;
+}
